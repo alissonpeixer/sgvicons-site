d638c0035a4fbd940dbd0e23fac3dbc0431d8f84
Alisson Peixer <alissonpeixer4@gmail.com>
2022-07-20 20:51:00 -0300

    :star: | Js Funcion Copy, emplementado

---
+++ js/index.js
@@ -0,0 +1,27 @@
+document.addEventListener('click', function(e){
+  const el = e.target;
+  if(el.classList.contains('icon')){
+    const el = e.target
+    const src = e.target.src
+    sucesso(el)
+    copySrc(src)
+  } 
+})
+
+function sucesso(el){
+  const msg = document.querySelector('.mensagem-sucesso');
+  msg.innerText =`Copiado o incone do ${el.alt}!`
+}
+
+function copySrc(src) {
+  var textToCopy = src;
+  var InputElement = document.createElement("input");
+  InputElement.type = "text";
+  InputElement.value = textToCopy;
+  document.body.appendChild(InputElement);
+  InputElement.select();
+  document.execCommand("Copy");
+  document.body.removeChild(InputElement);
+}
+
+
